b6ceea4be3b28a74938fdad483ee6ccab27b47c3
Prateek Nair <prtknr@gmail.com>
2026-05-26 12:58:42 +0530

    Assignment1

---
+++ javascript-training/language-basics/Assignment1.js
@@ -0,0 +1,124 @@
+//Assignments1
+//1. Temperature of a city in degrees Celsius: 25.5
+let temp="25.5";
+console.log(`Temperature of a city in degrees Celsius ${temp}`);
+
+// 2. Whether a customer has placed an order: true or false
+let orderStat1 = (true);
+let orderStat2 = (false);
+console.log(typeof orderStat2);
+console.log(`Whether a customer has placed an order: ${orderStat1}`)
+
+// 3. Person's phone number: "123-456-7890"
+
+let phonenumber=("123-456-7890");
+console.log(typeof phonenumber);
+console.log(`Person's phone number: ${phonenumber}`);
+
+// 4. Amount of money in a customer's bank account: 1000.50
+
+let Amount=(1000.50);
+console.log(typeof Amount);
+console.log(`Amount of money in a customer's bank account: ${Amount}`);
+
+//5. Person's email address: "john.doe@example.com"
+
+let email=("john.doe@example.com");
+console.log(typeof email);
+console.log(`Person's email address:${email}`);
+
+
+// 6. Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+let coordinates = {
+  latitude: 37.7749,
+  longitude: -122.4194
+};
+
+console.log(typeof coordinates);
+
+console.log(`Coordinates of a location (latitude, longitude):${coordinates.latitude} ${coordinates.longitude}`);
+
+// 7. Person's marital status: true or false
+
+let marstat1 = (true);
+let marstat2 = (false);
+console.log(typeof marstat1);
+console.log(typeof marstat2);
+console.log(`Person's marital status: ${marstat1}`);
+
+// 8. Person's occupation: "Software Engineer"
+
+let job=("Software Engineer");
+console.log(typeof job);
+console.log(`Person's occupation:${job}`);
+
+//9. Person's favourite colour: "Blue"
+
+let Colour=("Blue");
+console.log(typeof Colour);
+console.log(`Person's favourite colour: ${Colour}`);
+
+//10.Current year: 2023
+
+let Year=(2023);
+console.log(typeof Year);
+console.log(`Current year: ${Year}`);
+
+//11.Number of followers on a social media platform: 1,000,000
+let followercount=(1000000);
+console.log(typeof followercount);
+console.log(`Number of followers on a social media platform: ${followercount}`);
+
+// 12.Rating of a movie: 7.5
+
+let rating=(7.5);
+console.log(typeof rating);
+console.log(`Rating of a movie: ${rating}`);
+
+//13.Person's blood type: 'A'
+
+let btype=("A");
+console.log(typeof btype);
+console.log(`Person's blood type:  ${btype}`);
+
+//14.Title of a book: "To Kill a Mockingbird"
+let booktitle=("To Kill a Mockingbird");
+console.log(typeof booktitle);
+console.log(`Title of a book:  "${booktitle}"`);
+
+//15.Number of employees in a company: 500
+let empcount=(500);
+console.log(typeof empcount);
+console.log(`Number of employees in a company:  ${empcount}`);
+
+//16.Time of an event: 2:30 PM
+
+let time=("2:30");
+console.log(typeof time);
+console.log(`Time of an event: ${time} PM`);
+
+//17.Name of a country: "United States"
+
+let country=("United States");
+console.log(typeof country);
+console.log(`Name of a country: ${country}`);
+
+//18.Person's eye color: "Brown"
+
+let color=("Brown");
+console.log(typeof color);
+console.log(`Person's eye color:  ${color}`);
+
+
+//19.Person's birthplace: "New York City"
+
+let birthplace=("New York City");
+console.log(typeof birthplace);
+console.log(`Person's birthplace: ${birthplace}`);
+
+//20. Distance between two cities: 200.5
+
+let distance=(200.5);
+console.log(typeof distance);
+console.log(`Distance between two cities: ${distance}`);
+
