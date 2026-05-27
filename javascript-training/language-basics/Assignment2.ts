5ca58151614f4beab437aea9ed97e58849f022ad
Prateek Nair <prtknr@gmail.com>
2026-05-27 23:32:51 +0530

    Assignment2.ts

--- javascript-training/language-basics/Assignment2.ts
+++ javascript-training/language-basics/Assignment2.ts
@@ -3,7 +3,7 @@
 let AccountHolderdetails
 let customername: string = "Jhon Doe";
 let CreditScore: number = 750;
-let Salary: number = 50000;
+let Salary: number = 40000;
 let isemployed: boolean = false;
 let DTI: number = 30;
 
@@ -13,23 +13,24 @@ if (CreditScore > 750) {
 }
 else if (CreditScore >= 650 && CreditScore === 750) {
     console.log("Verify customer’s income must be at least $50,000")
+    if (Salary >= 50000) {
+        console.log("Verify customer is employed or not ")
+        if (!isemployed) {
+            console.log("Customer is not employed & not eligible for loan")
+        } else {
+            console.log("Customer is employed")
+        }
+
+        if (DTI < 40) {
+            console.log("DTI ratio is more than 40%,")
+        } else {
+            console.log("DTI ratio is less than 40%, ")
+
+        }
+
+    }
+
 }
 else {
     console.log("credit score is below 650, the loan is denied.")
-}
-if (Salary >= 50000) {
-    console.log("Verify customer is employed or not ")
-    if (!isemployed ) {
-    console.log("Customer is not employed & not eligible for loan")
-    }else{
-        console.log("Customer is employed")
-    }
-
-    if (DTI < 40) {
-    console.log("DTI ratio is less than 40%,")
-} else {
-    console.log("DTI ratio is less than 40%, ")
-
-}
-
 }
