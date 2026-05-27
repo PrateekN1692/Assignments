732b91caeb9ecf7075eea10a30d10cfb4c139e65
Prateek Nair <prtknr@gmail.com>
2026-05-27 23:11:48 +0530

    Assignment2.ts

--- javascript-training/language-basics/Assignment2.ts
+++ javascript-training/language-basics/Assignment2.ts
@@ -4,34 +4,32 @@ let AccountHolderdetails
 let customername: string = "Jhon Doe";
 let CreditScore: number = 750;
 let Salary: number = 50000;
-let isemployed: boolean = true;
+let isemployed: boolean = false;
 let DTI: number = 30;
 
 
-if (CreditScore >= 750) {
+if (CreditScore > 750) {
     console.log("Loan is approved")
 }
-else if (CreditScore >= 650 && CreditScore < 750) {
+else if (CreditScore >= 650 && CreditScore === 750) {
     console.log("Verify customer’s income must be at least $50,000")
 }
 else {
     console.log("credit score is below 650, the loan is denied.")
 }
 if (Salary >= 50000) {
-    console.log("customer is elegible for Loan")
+    console.log("Verify customer is employed or not ")
+    if (!isemployed ) {
+    console.log("Customer is not employed & not eligible for loan")
+    }else{
+        console.log("Customer is employed")
+    }
+
+    if (DTI < 40) {
+    console.log("DTI ratio is less than 40%,")
 } else {
-    console.log("customer is not elegible for Loan")
+    console.log("DTI ratio is less than 40%, ")
+
 }
-if (isemployed = true) {
-    console.log("Customer is employed eligible for loan")
-} else {
-    console.log("Customer is not employed he is not eligible for loan")
 
-} if (DTI < 40) {
-    console.log("DTI ratio is less than 40%, the loan is approved.")
-} else {
-    console.log("DTI ratio is less than 40%, the loan is approved.")
 }
-
-
-
